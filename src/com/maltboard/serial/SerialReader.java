6d94d7b0afb8718dcef17bff82a00d6ee90c2d61
Yuri Pourre <yuriamayuri@gmail.com>
2015-05-14 16:43:42 -0400

    Refactoring

---
+++ src/com/maltboard/serial/SerialReader.java
@@ -0,0 +1,118 @@
+package com.maltboard.serial;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEventListener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+public class SerialReader {
+	private static final String PORT_NAMES[] = { 
+		"/dev/tty.usbserial-A9007UX1", // Mac OS X
+		"/dev/ttyACM0", // Raspberry Pi
+		"/dev/ttyACM1",
+		"/dev/ttyUSB0", // Linux
+		"COM3", // Windows
+	};
+
+	private SerialPort serialPort;
+	/** The port we're normally going to use. */
+
+	/**
+	 * A BufferedReader which will be fed by a InputStreamReader 
+	 * converting the bytes into characters 
+	 * making the displayed results codepage independent
+	 */
+	private BufferedReader input;
+	/** The output stream to the port */
+	private OutputStream output;
+	/** Milliseconds to block while waiting for port open */
+	private static final int TIME_OUT = 2000;
+	/** Default bits per second for COM port. */
+	private static final int DATA_RATE = 9600;
+
+	public void init(SerialPortEventListener listener) {
+		
+		configureSerial();
+
+		CommPortIdentifier portId = findPort();
+		
+		if (portId == null) {
+			System.out.println("Could not find COM port.");
+			return;
+		}
+
+		openPort(listener, portId);
+	}
+
+	private void openPort(SerialPortEventListener listener,
+			CommPortIdentifier portId) {
+		try {
+			// open serial port, and use class name for the appName.
+			serialPort = (SerialPort) portId.open(this.getClass().getName(),
+					TIME_OUT);
+
+			// set port parameters
+			serialPort.setSerialPortParams(DATA_RATE,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			// open the streams
+			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			output = serialPort.getOutputStream();
+
+			// add event listeners
+			serialPort.addEventListener(listener);
+			serialPort.notifyOnDataAvailable(true);
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	private CommPortIdentifier findPort() {
+		CommPortIdentifier portId = null;
+		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+		//First, Find an instance of serial port as set in PORT_NAMES.
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			for (String portName : PORT_NAMES) {
+				if (currPortId.getName().equals(portName)) {
+					portId = currPortId;
+					break;
+				}
+			}
+		}
+		return portId;
+	}
+
+	private void configureSerial() {
+		// the next line is for Raspberry Pi and 
+		// gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+		System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+	}
+
+	/**
+	 * This should be called when you stop using the port.
+	 * This will prevent port locking on platforms like Linux.
+	 */
+	public synchronized void close() {
+		if (serialPort != null) {
+			serialPort.removeEventListener();
+			serialPort.close();
+		}
+	}
+
+	public BufferedReader getInput() {
+		return input;
+	}
+
+	public OutputStream getOutput() {
+		return output;
+	}
+
+}
