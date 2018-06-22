05037b5b639acca78ace0340a685719952aa0425
Yuri Pourre <yuripourre@gmail.com>
2018-06-22 15:16:38 -0300

    Allows manual port selection

--- src/main/java/com/harium/malt/serial/Serial.java
+++ src/main/java/com/harium/malt/serial/Serial.java
@@ -14,56 +14,65 @@ public class Serial implements SerialPortEventListener {
     public static boolean DEBUG = false;
 
     private static final String PORT_NAMES[] = {
-            "/dev/tty.usbserial-A9007UX1", // Mac OS X
-            "/dev/ttyACM0", // Raspberry Pi
-            "/dev/ttyACM1",
-            "/dev/ttyUSB0", // Linux
-            "COM3", // Windows
+            "/dev/tty.usbserial*", // Mac OS X
+            "/dev/ttyACM\\d+", // Raspberry Pi
+            "/dev/ttyUSB\\d+", // Linux
+            "COM\\d+", // Windows
     };
 
     private SerialListener listener;
 
+    /**
+     * The port we're normally going to use.
+     */
     private SerialPort serialPort;
-    /** The port we're normally going to use. */
 
     /**
      * A BufferedReader which will be fed by a InputStreamReader
      * converting the bytes into characters
      * making the displayed results codepage independent
      */
-    private BufferedReader input;
+
+    protected BufferedReader input;
     /**
      * The output stream to the port
      */
-    private OutputStream output;
+    protected OutputStream output;
+
     /**
      * Milliseconds to block while waiting for port open
      */
-    private static final int TIME_OUT = 2000;
+    private static final int TIME_OUT = 5000;
+
     /**
      * Default bits per second for COM port.
      */
     private static final int DATA_RATE = 9600;
 
-    int baudRate = DATA_RATE;
+    protected String port = "";
+    protected int baudRate = DATA_RATE;
 
     public Serial() {
         super();
     }
 
-    public Serial(int baudRate) {
+    public Serial(String port, int baudRate) {
         super();
-
+        this.port = port;
         this.baudRate = baudRate;
     }
 
-    public void init(final SerialListener listener) {
+    public Serial(int baudRate) {
+        super();
+        this.baudRate = baudRate;
+    }
 
+    public void initAsync(final SerialListener listener) {
         new Thread(new Runnable() {
 
             public void run() {
                 try {
-                    initDebug(listener);
+                    init(listener);
                 } catch (PortInUseException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -81,7 +90,7 @@ public class Serial implements SerialPortEventListener {
         }).start();
     }
 
-    public void initDebug(SerialListener listener) throws PortInUseException, UnsupportedCommOperationException, IOException, TooManyListenersException {
+    public void init(SerialListener listener) throws PortInUseException, UnsupportedCommOperationException, IOException, TooManyListenersException {
         this.listener = listener;
 
         configureSerial();
@@ -116,20 +125,36 @@ public class Serial implements SerialPortEventListener {
         output = serialPort.getOutputStream();
     }
 
-    private CommPortIdentifier findPort() {
+    /* package */ CommPortIdentifier findPort() {
         CommPortIdentifier portId = null;
         Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 
+        if (!port.isEmpty()) {
+            while (portEnum.hasMoreElements()) {
+                CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+                if (port.equals(currPortId.getName())) {
+                    portId = currPortId;
+                }
+            }
+        } else {
+            findPort(portEnum, PORT_NAMES);
+        }
+        return portId;
+    }
+
+    /* package */ static CommPortIdentifier findPort(Enumeration<CommPortIdentifier> portEnum, String[] regexList) {
+        CommPortIdentifier portId = null;
         //First, Find an instance of serial port as set in PORT_NAMES.
         while (portEnum.hasMoreElements()) {
-            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-            for (String portName : PORT_NAMES) {
-                if (currPortId.getName().equals(portName)) {
+            CommPortIdentifier currPortId = portEnum.nextElement();
+            for (String regex : regexList) {
+                if (currPortId.getName().matches(regex)) {
                     portId = currPortId;
                     break;
                 }
             }
         }
+
         return portId;
     }
 
@@ -152,14 +177,6 @@ public class Serial implements SerialPortEventListener {
             serialPort.removeEventListener();
             serialPort.close();
         }
-    }
-
-    public BufferedReader getInput() {
-        return input;
-    }
-
-    public OutputStream getOutput() {
-        return output;
     }
 
     /**
@@ -185,15 +202,28 @@ public class Serial implements SerialPortEventListener {
      * Handle an event on the serial port. Read the data and print it.
      */
     public synchronized void serialEvent(SerialPortEvent oEvent) {
-        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-            try {
-                receive();
-            } catch (Exception e) {
-                listener.error(e);
-                System.err.println(e.toString());
-            }
+        switch (oEvent.getEventType()) {
+            case SerialPortEvent.DATA_AVAILABLE:
+                try {
+                    receive();
+                } catch (Exception e) {
+                    listener.error(e);
+                    System.err.println(e.toString());
+                }
+                break;
+            // Ignore all the other eventTypes, but you should consider the other ones.
+            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+            case SerialPortEvent.CTS:
+            case SerialPortEvent.DSR:
+            case SerialPortEvent.RI:
+            case SerialPortEvent.CD:
+            case SerialPortEvent.OE:
+            case SerialPortEvent.PE:
+            case SerialPortEvent.FE:
+            case SerialPortEvent.BI:
+            default:
+                break;
         }
-        // Ignore all the other eventTypes, but you should consider the other ones.
     }
 
 }
--- src/test/java/com/harium/malt/serial/SerialTest.java
+++ src/test/java/com/harium/malt/serial/SerialTest.java
@@ -1,7 +1,18 @@
 package com.harium.malt.serial;
 
+import gnu.io.CommPortIdentifier;
+import gnu.io.Config;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SerialTest {
 
@@ -9,6 +20,62 @@ public class SerialTest {
     public void testInit() {
         Serial serial = new Serial(9600);
         Assert.assertEquals(9600, serial.baudRate);
+    }
+
+    @Test
+    public void testFindWindowsPortWithRegex() {
+        // Windows port
+        CommPortIdentifier windowsPort = mock(CommPortIdentifier.class);
+        when(windowsPort.getName()).thenReturn("COM3");
+
+        List<CommPortIdentifier> portList = new ArrayList<>();
+        portList.add(windowsPort);
+        Enumeration ports = Collections.enumeration(portList);
+
+        CommPortIdentifier found = Serial.findPort(ports, new String[]{"COM\\d+"});
+        Assert.assertEquals(windowsPort, found);
+    }
+
+    @Test
+    public void testMacPortWithRegex() {
+        // MacOS port
+        CommPortIdentifier macPort = mock(CommPortIdentifier.class);
+        when(macPort.getName()).thenReturn("/dev/tty.usbserial-mydevice");
+
+        List<CommPortIdentifier> portList = new ArrayList<>();
+        portList.add(macPort);
+        Enumeration ports = Collections.enumeration(portList);
+
+        CommPortIdentifier found = Serial.findPort(ports, new String[]{"/dev/tty.usbserial*"});
+        Assert.assertEquals(macPort, found);
+    }
+
+    @Test
+    public void testRaspberryPiPortWithRegex() {
+        // Raspberry Pi port
+        CommPortIdentifier macPort = mock(CommPortIdentifier.class);
+        when(macPort.getName()).thenReturn("/dev/ttyACM0");
+
+        List<CommPortIdentifier> portList = new ArrayList<>();
+        portList.add(macPort);
+        Enumeration ports = Collections.enumeration(portList);
+
+        CommPortIdentifier found = Serial.findPort(ports, new String[]{"/dev/ttyACM\\d+"});
+        Assert.assertEquals(macPort, found);
+    }
+
+    @Test
+    public void testLinusPiPortWithRegex() {
+        // Linux port
+        CommPortIdentifier macPort = mock(CommPortIdentifier.class);
+        when(macPort.getName()).thenReturn("/dev/ttyUSB0");
+
+        List<CommPortIdentifier> portList = new ArrayList<>();
+        portList.add(macPort);
+        Enumeration ports = Collections.enumeration(portList);
+
+        CommPortIdentifier found = Serial.findPort(ports, new String[]{"/dev/ttyUSB\\d+"});
+        Assert.assertEquals(macPort, found);
     }
 
 }
